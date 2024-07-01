5a2188fc39b88e58d3713c1154ab3604b66cabc0
Yash Pandey <yashdevesh10@gmail.com>
2024-07-01 13:16:09 +0530

    XDisplayName

--- XStopWatch/src/App.js
+++ XStopWatch/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import Xcountries from "./Components/XCountries";
 import XStopWatch from "./Components/XStopWatch";
 import Sample from "./Components/Sample";
+import XDisplayName from "./Components/XDisplayName";
 
 const App = () => {
   return (
     <>
       {/* <Xcountries/> */}
-      <XStopWatch/>
+      {/* <XStopWatch/> */}
       {/* <Sample/> */}
+      <XDisplayName/>
     </>
   );
 }
