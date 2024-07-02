b46a6d51a280751a7e543b9c754e4da1e65db27b
Yash Pandey <yashdevesh10@gmail.com>
2024-07-02 11:45:59 +0530

    XCounter

---
+++ XCounter/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Xcountries from "./Components/XCountries";
+import XStopWatch from "./Components/XStopWatch";
+import XDisplayName from "./Components/XDisplayName";
+import XCounter from "./Components/XCounter";
+
+const App = () => {
+  return (
+    <>
+      {/* <Xcountries/> */}
+      {/* <XStopWatch/> */}
+      {/* <XDisplayName/> */}
+      <XCounter/>
+    </>
+  );
+}
+
+export default App;
